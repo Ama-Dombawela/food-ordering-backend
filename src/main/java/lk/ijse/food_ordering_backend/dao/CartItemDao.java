b4be34eee76f2d07fcec915cad57180fb948ec00
Ama-Dombawela <amadombawela@gmail.com>
2026-05-10 03:22:38 +0530

    refactor(dao): change CartDao to findByUser_Id() and CartItemDao to findByCart_Id()

--- src/main/java/lk/ijse/food_ordering_backend/dao/CartItemDao.java
+++ src/main/java/lk/ijse/food_ordering_backend/dao/CartItemDao.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import lk.ijse.food_ordering_backend.entity.CartItem;
 
+// Handles database access for cart items.
 @Repository
 public interface CartItemDao extends JpaRepository<CartItem, Long> {
-    List<CartItem> findByCartId(Long cartId);
+    List<CartItem> findByCart_Id(Long cartId);
 }
