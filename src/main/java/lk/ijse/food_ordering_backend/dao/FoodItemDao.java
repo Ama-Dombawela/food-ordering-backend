630197f3a33052ce94f276815bc768783593e68b
Ama-Dombawela <amadombawela@gmail.com>
2026-05-10 03:21:43 +0530

    Change FoodItemDao method to findByCategory_Id()

--- src/main/java/lk/ijse/food_ordering_backend/dao/FoodItemDao.java
+++ src/main/java/lk/ijse/food_ordering_backend/dao/FoodItemDao.java
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Repository;
 import lk.ijse.food_ordering_backend.entity.FoodItem;
 import lk.ijse.food_ordering_backend.entity.FoodItemStatus;
 
+// Handles database access for food items.
 @Repository
 public interface FoodItemDao extends JpaRepository<FoodItem, Long> {
     List<FoodItem> findByStatus(FoodItemStatus status);
 
-    List<FoodItem> findByCategoryId(Long categoryId);
+    List<FoodItem> findByCategory_Id(Long categoryId);
 }
